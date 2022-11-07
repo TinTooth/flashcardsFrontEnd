01a6a352ae5fec3333a984cd40b0a983a50a84c3
TinTooth <boconne4@gmail.com>
2022-11-07 15:12:49 -0600

    Created getAllCollections + getFlashCards()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 
 function App() {
+  const [collections,setCollections] = useState([]);
+  const [flashCards,setFlashCards] = useState([]);
+
+  useEffect(() => {
+    getAllCollections();
+  },[]);
+
+
+  async function getAllCollections() {
+    const response = await axios.get('http://127.0.0.1:8000/api/collections/');
+    setCollections(response.data)
+  }
+
+  async function getFlashCards(collectionID) {
+    endpoint = 'http://127.0.0.1:8000/api/collections/' + collectionID +'/cards/';
+    const response = await axios.get(endpoint);
+    setFlashCards(response.endpoint)
+  }
+
+
   return (
     <div>
       <Footer />
