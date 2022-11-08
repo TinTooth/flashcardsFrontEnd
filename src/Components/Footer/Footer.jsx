a5c204a94d854a696871bf67d548dcc606aaa0c3
TinTooth <boconne4@gmail.com>
2022-11-08 15:10:49 -0600

    Added card management Modals to Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import Modal from '../Modal/Modal';
 const Footer = (prop) => {
     const [showAdd,setShowAdd] = useState(false)
     const [showEdit,setShowEdit] = useState(false)
+    const [showDelete,setShowDelete] = useState(false)
 
     const handleAdd = () => {
         if (!showAdd) {setShowAdd(true);}
@@ -14,17 +15,24 @@ const Footer = (prop) => {
         if (!showEdit) {setShowEdit(true);}
         else {setShowEdit(false);}
     }
+    const handleDelete = () => {
+        if (!showDelete) {setShowDelete(true);}
+        else {setShowDelete(false);}
+    }
+    
 
     return ( 
         <div className='footer'>
             <Modal title = 'Add Card' onClose={handleAdd} show ={showAdd}>Test Child</Modal>
-            <Modal title = 'Edit Card' oncClose = {handleEdit} show ={showEdit}>Test</Modal>
+            <Modal title = 'Edit Card' onClose = {handleEdit} show ={showEdit}>Test</Modal>
+            <Modal title = 'Edit Card' onClose = {handleDelete} show ={showDelete}>Tester</Modal>
             <div className='brand'>Brand</div>
             
             <div className='edits'>
                 <div  className = 'edit-card'onClick = {handleAdd}>Add Card</div>
                 <div  className = 'edit-card'onClick = {handleEdit}>Edit Card</div>
-                <div>Delete Card</div>
+                <div  className = 'edit-card'onClick = {handleDelete}>Delete Card</div>
+                
             </div>
             <div className="invs">Brand</div>
         </div>
